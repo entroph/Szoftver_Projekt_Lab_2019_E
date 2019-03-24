54c720211d08a69725b5f76cef0df67badefadea
mrtnvrga <vaxell@gmail.com>
2019-03-24 19:32:45 +0100

    compile.bat cleaned

--- compile.bat
+++ compile.bat
@@ -1,11 +1,6 @@
 @echo off
 set path=%path%;C:\Program Files\Java\jdk1.7.0_80\bin
-mkdir class
-
 javac -encoding UTF-8 -d . src\skeleton\*.java
-
 jar cmf manifest.mf pandaPlaza.jar skeleton\*.class
-pause
-
-
+pause
 @echo Compiled successfully.
